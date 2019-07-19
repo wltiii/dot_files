41634b87da614ba757fa7f7ca7769cc7c256d911
bturner <bill.turner@windlogics.com>
2019-07-19 11:50:30 -0500

    branching issue

--- make-project.sh
+++ make-project.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
 # creates a new local and remote branch from the story id & description
 
@@ -8,53 +8,50 @@
 # https://stackoverflow.com/questions/51811227/how-to-create-a-local-repository-and-remote-repository-in-gitlab-using-command-l
 # https://stackoverflow.com/questions/33101962/how-to-create-a-new-gitlab-repo-from-my-existing-local-git-repo-using-cli
 
-#TODO get id and name from args
-# id="ENOP-895"
-# echo $id
-# name="Switch Ionic app to use Gexa API proxy"
-# echo $name
+# TODO need a better way to handle origin branches. i.e. if one does not exist, use the other
+# and be able to pass in a 'develop' branch name
 develop_branch="develop"
 echo $develop_branch
 master_branch="master"
 echo $master_branch
+
 # TODO build branch name - right now, use make-branch_name and pass that in
 if [ "$#" -lt 1 -o "$#" -gt 2 ]; then
     echo "Illegal number of parameters"
     exit
 fi
+
 # i tend to forget how to do command substitution using vars that contain space. see:
 # https://stackoverflow.com/a/54438246/471133
 if [ "$#" -eq 2 ]; then
-    # branch_name="$(make-branch-name $1 $2)"
     command=(make-branch-name "$1" "$2")
 elif [ "$#" -eq 1 ]; then
-    # branch_name="$(make-branch-name $1)"
     command=(make-branch-name "$1")
-# else
-#     echo "Illegal number of parameters"
-#     exit
 fi
-echo "${command[@]}"
-# command=(make-branch-name "$@")
-# branch_name="$command[@]"
-# branch_name=$command
+
+# make the branch name
 branch_name=$("${command[@]}")
-# make-branch-name
-# echo $1
-# branch_name="$1"
-echo $branch_name
-# TODO attempt to checkout develop first, then master, set working-branch to that which is checked out
+
+# checkout the develop branch
+# TODO attempt to checkout develop first, then master, set working-branch to that which is
+# checked out. Handling the pathspec issue below should do it
 # TODO handle the following:
 # ? git checkout zzzz
 # error: pathspec 'zzzz' did not match any file(s) known to git.
-# NOTE: should probably have these roots configurable, maybe not allow fallback
-# git checkout $develop_branch
-# git pull
-# # branch working directory and checkout
-# git checkout -b $branch_name
-# # commit the project - NOTE: this depends upon my function
-# gc "story initialized"
+git checkout $develop_branch
+
+# update the the branch
+git pull
+
+# branch working directory using the new branch name and checkout
+echo "branch_name is [${branch_name}]"
+git branch $branch_name
+git checkout $branch_name
+
+# # commit the project - NOTE: this depends upon my gc function
+gc "story initialized"
+
 # # push to origin
 # # NOTE: if this does not work, see: https://stackoverflow.com/a/6089415/471133
 # # git push origin --set-upstream origin $branch_name
-# git push -u origin $branch_name
+git push -u origin $branch_name
