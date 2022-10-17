0eb9b555fda93761d3e206eb6ebc4e38cec987c0
wltiii <myX2bear>
2022-10-16 22:12:56 -0500

    added library (barrel) file stub creation

--- scripts/bash/make-dart-package.sh
+++ scripts/bash/make-dart-package.sh
@@ -16,6 +16,7 @@
 # 5) commit
 # 6) push to github (with config for repo)
 # 7) generate with alternate license
+# 8) create barrel file stub
 
 makePath() {
   path="$1"
@@ -127,6 +128,35 @@ addGitIgnore() {
   addFile $path $FILE "$CONTENT"
 }
 
+addMainLibraryFileStub() {
+  path="$1"
+
+  FILE="$path.dart"
+  CONTENT="/// This is the main library (barrel) file. It is placed directly under lib.
+/// Use it to provide developers with an overview of the entire public API of the package.
+/// Also, to avoid exposing more API than intended.
+///
+/// Add exports and use show to specify exactly which symbols you wish to expose. For example,
+/// given you wish to expose file 'add_chunked_encoding.dart' as 'addChunkedEncoding', and this
+/// file is located in folder 'middleware' which itself is located in the 'src' folder, you
+/// would write the following statement.
+///
+/// \`\`\`dart
+/// export 'src/middleware/add_chunked_encoding.dart' show addChunkedEncoding;
+/// \`\`\`
+///
+/// For more about organizing a library file, see:
+///   https://dart.dev/guides/libraries/create-library-packages#organizing-a-library-package
+///
+/// Document the package api well. See:
+///   https://dart.dev/guides/libraries/create-library-packages#documenting-a-library and
+///   https://dart.dev/guides/language/effective-dart/documentation
+///
+"
+
+  addFile $path $FILE "$CONTENT"
+}
+
 addFile() {
   path="$1"
   file="$2"
@@ -161,6 +191,7 @@ doMake() {
   addMITLicense $pkg
   addReadMe $pkg
   addGitIgnore $pkg
+  addMainLibraryFileStub $pkg
 }
 
 package="$1"
