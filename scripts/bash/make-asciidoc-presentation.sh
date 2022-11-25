096bb502ebd54f4b3e41fb86c26cc6dd4261fe9c
wltiii <myX2bear>
2022-11-25 11:30:42 -0600

    renamed: reworked make-asciidoc-presentation to use node/npm as rvm does not work on current Ubuntu. Added more steps to create folders and presentation stub, validation, verification and documentation.

--- scripts/bash/make-asciidoc-presentation.sh
+++ scripts/bash/make-asciidoc-presentation.sh
@@ -3,106 +3,218 @@
 # performs setup for an asciidoc slides presentation
 #
 # USAGE:
-#     edit the list of repos to be created at the bottom of this script.
-#     Run command:
-#         make-asciidoc-presentation.sh
+#     make-asciidoc-presentation.sh
 #
 
 setup() {
-#  preamble
-#  verifyPath
-  verifyRvmIsInstalled
+  ROOT=$(pwd)
+#  PRESENTATION_DIR='DELETE_ME'
+#  PRESENTATION_ASSETS='DELETE_ME_TOO'
+  PRESENTATION_DIR='doc'
+  PRESENTATION_ASSETS='assets'
+
+  preamble
+  verifyPath
+  stubPresentation
+  verifyNpmInstallation
+  installNpmDependency
+  verifyRevealJsInstallation
 }
 
 preamble() {
   echo "Script asciidoc-slides-setup must be run from the root of your project.
 
-It does the following:
- - verifies this script is running from the project directory
- - checks that ruby version manager is installed
- - installs latest ruby version
- - installs reveal js in project directory ???
+NOTE: This script assumes you are co-mingling a presentation with application source
+code. It further assumes the presentation source will exist in folder 'doc', and
+that folder is a direct child of the project root. It also creates a presentation
+'assets' folder within the doc folder. Thus, you will end up with:
+
++ {project root}
+|
++-+ doc
+  |
+  +-+ assets
+
+This script does the following:
+ - verifies the script is running from the application project root directory
+ - creates a doc/assets directories if they do not already exist
+ - verifies node and npm have been installed
+ - creates a presentation.adoc stub in the doc folder if it does not already exist
+ - installs npm dependency at the root of the presentation directory ???
+ - installs reveal js in project directory
 "
 }
 
 verifyPath() {
-  var=$(pwd)
-  echo "Is $var your project root directory? (y/n)"
+  echo ""
+  echo "path verification..."
+
+  echo "Is your application project root $ROOT? (y/n)"
   read response
 
   if [ "$response" != 'y' ]; then
-    echo "Navigate to the root directory of your poject and re-run."
+    echo "Navigate to the root directory of your application project and re-run."
     exit
   fi
 
-#  echo "You responded with $response"
+  echo "Application project root is $ROOT..."
 }
 
-verifyRvmIsInstalled() {
-  DATE=`date -I`
-  echo "DATE is"
-  echo $DATE
-  RAWTYPE=`type rvm`
-  echo "RAWTYPE is $RAWTYPE"
-  TYPE=$(type rvm 2>&1)
-  echo "TYPE is $TYPE"
-#  echo $TYPE
+stubPresentation() {
+  echo "Stubbing presentation into $ROOT..."
 
-#  OUT=$(type rvm | head -n 1 2>&1)
-#  OUT=$(type rvm | head -n 1)
-  echo "$(type rvm | head -n 1)"
-  OUT=`type rvm | head -n 1`
-  # head: option requires an argument -- n usage: head [-n lines | -c bytes] [file ...]
-  echo "OUT is"
-  echo "$OUT"
-  echo $OUT
-  echo OUT
-  echo "####### piped ##############"
-filename='weekday.txt'
-while read line; do
-day=`echo $line | cut -c 1-3`
-if [ $day == "Sun" ]
-then
-echo "Sunday is the holiday"
-else
-echo $day
-fi
-done<$filename  echo "#########################"
-  RVM=$(type rvm)
-  echo "RVM is $RVM"
-#  type rvm >> RESULT
-#  RESULT = echo "type rvm"
-  echo "###### type rvm ###################"
-  echo "type rvm"
-  echo "$(type rvm)"
-  echo "###### result ###################"
-  RESULT = echo "$(type rvm)"
-#  RESULT = echo "head -n 1 $RVM"
-  echo "RESULT is $RESULT"
-  echo "###### head ###################"
-  head -n 1 $HOME/.rvm/bin/rvm
-#  echo `head -n 1 $HOME/.rvm/bin/rvm`
-  HEAD=$(echo `head -n 1 $HOME/.rvm/bin/rvm`)
-  echo $HEAD
-  echo "#########################"
+  cd $ROOT
 
-#  var="$(type rvm | head -n 1)"
+  if [[ -d "$PRESENTATION_DIR" ]]; then
+    echo "Presentation folder $PRESENTATION_DIR already exists."
+  else
+    echo "Presentation folder does not exist. Creating tree..."
+    mkdir -p -- "$PRESENTATION_DIR"
+    mkdir -p -- "$PRESENTATION_DIR/$PRESENTATION_ASSETS"
+  fi
 
-#  echo "e is ${e}"
-#  echo "RVM is ${RVM}"
-#  echo "var is ${var}"
+  if [[ -d "$PRESENTATION_DIR/$PRESENTATION_ASSETS" ]]; then
+    echo "Presentation $PRESENTATION_ASSETS folder already exists."
+  else
+    echo "Folder $PRESENTATION_ASSETS does not exist. Creating..."
+    mkdir -p -- "$PRESENTATION_DIR/$PRESENTATION_ASSETS"
+  fi
 
-#  if [ "${e}" -ge '1' ]; then
-#    echo "Foo returned BAD exit code ${e}"
-#  fi
+  if [[ -f "$PRESENTATION_DIR/presentation.adoc" ]]; then
+    echo "Presentation $PRESENTATION_ASSETS/presentation.adoc folder already exists. Not created."
+  else
+    echo "Presention presentation.adoc does not exist. Creating..."
+    # using heredoc to create file
+    cat <<END >$PRESENTATION_DIR/presentation.adoc
+= Flutter - Adaptive and Responsive
+Version 0.1, 11/19/2022
 
-#  echo "RVM verification result is var"
+:description: Presentation of Flutter Adaptive and Reponsive practices and concerns
+:authors: Manoj Sahu (c) and Bill Turner (c)
+:sourcedir: ../lib
+:imagesdir: ./assets
+:source-highlighter: pygments
 
-#  if [ "$var != rvm is a function" ]; then
-#    echo "RVM is not installed. Click on https://rvm.io/rvm/install for instructions."
-#    exit
-#  fi
+== A Slide With Text
 
+Blah blah blah
+
+== An Image
+
+=== A Sahu / Turner Presentation
+
+.Bill and Manoj in Mallorca, May 2002
+image::mallorca010.JPG[]
+
+== Some Source Code
+
+This is what I want to show.
+
+[source, dart]
+----
+include::{sourcedir}/example_1.dart[]
+----
+
+== Quoting
+
+A quote
+
+[quote, Tim Sneath, Googlie I/O Developer Conference: 05/11/2022]
+We're announcing Flutter 3, which is the culmination of our journey to delivering multiplatform UI development across
+phone and desktop and web
+
+<<<
+
+Does anything more need be said?
+
+== References
+
+html links
+
+== Contact Links
+
+LinkedIn or whatever
+
+== Thank You!
+
+You can find this presentation and all examples at:
+
+github link
+END
+  fi
+
+  echo "Presentation directories and default doc stubbed..."
+}
+
+# Where is this being installed? Presumable in $USER
+verifyNpmInstallation() {
+  echo "Verifying node and npm are installed..."
+
+  NODE=`which node`
+  NPM=`which npm`
+
+  if [ -z "$NODE" ]; then
+    echo "============================================================================="
+    echo " NODE is not installed. See https://nodejs.org/en/ for more information."
+    echo " It is recommended to install node via nvm. See https://github.com/nvm-sh/nvm"
+    echo "============================================================================="
+    exit
+  fi
+  echo "NODE is $NODE"
+
+  if [ -z "$NPM" ]; then
+    echo "NPM is not installed. See https://nodejs.org/en/ for more information."
+    exit
+  fi
+  echo "NPM is $NPM"
+}
+
+# install the dependencies in a project directory, such as the directory where
+# your AsciiDoc presentation is stored
+# TODO does project directory mean the location of the adoc presentation? Assuming so...
+installNpmDependency() {
+  echo ""
+  cd $ROOT/$PRESENTATION_DIR
+
+  echo "installing npm dependencies..."
+
+
+  # TODO if package.json exists, do not run init
+  if [[ -f "package.json" ]]
+  then
+    echo "File package.json exists. Bypassing npm initialization."
+  else
+    npm init -y
+    echo "npm initialized..."
+  fi
+
+  echo ""
+  echo "installing asciidoctor dependencies..."
+  npm i --save asciidoctor @asciidoctor/reveal.js
+  echo "asciidoctor dependencies installed..."
+}
+
+# verify that the asciidoctor-revealjs command is available
+verifyRevealJsInstallation() {
+  echo ""
+  cd $ROOT/$PRESENTATION_DIR
+  echo "Verifying RevealJsInstallation. PWD is $PWD"
+
+  REVEAL=`npx asciidoctor-revealjs --version`
+
+  EXPECTED='''Asciidoctor reveal.js'''
+
+  echo $REVEAL
+
+  if [[ $REVEAL != $EXPECTED* ]]; then
+    echo """RevealJs is not installed properly. Expected validation string to
+    start with: [Asciidoctor reveal.js]. Instead, it returned:
+
+    $REVEAL
+    """
+  else
+    echo "RevealJs installed..."
+  fi
 }
 
 # Run the script
