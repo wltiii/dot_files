e6cd9457cf55561953c4268600cc3d8598e27feb
wltiii <myX2bear>
2023-11-15 21:21:55 -0600

    added Mac python alias added sourcing of private data updated dot_files

--- install.sh
+++ install.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 
-# copies `bash` profile files to home directory, backing up
+# Copies `bash` profile files to home directory, backing up
 # existing dot files to the dot_files_backup dir in users home
 
-DOT_FILES_BACKUP_HOME=~/dot_files_backup
+DOT_FILES_BACKUP_HOME="$HOME/dot_files_backup"
 
 TIMESTAMP=$(date "+%Y-%m-%dT%H:%M:%S")
 
@@ -14,54 +14,66 @@ createBackupDir() {
 }
 
 backupFile() {
-  source=~/"$1"
-  target="$source-$TIMESTAMP"
-  echo "backing up file $source to $DOT_FILES_BACKUP_HOME"
+  source="$HOME/$1"
+  target="$DOT_FILES_BACKUP_HOME/$1-$TIMESTAMP"
+  echo ".... backing up file $source to $DOT_FILES_BACKUP_HOME"
 
   if [ -f "$source" ]; then
     \cp -av -- "$source" "$target"
-    \mv -v "$target" "$DOT_FILES_BACKUP_HOME"
   fi
 }
 
 putFile() {
-  \cp -v "$1" ~/"$1"
+  if [ -f "$1" ]; then
+    \cp -v "$1" "$HOME/"
+  else
+    echo ".... source file $1 not found, skipping"
+  fi
 }
 
 installFile() {
   filename="$1"
-  targetFile=~/"$filename"
-  echo "installing $filename to $targetFile"
+  targetFile="$HOME/$filename"
+  echo ".... installing $filename to $targetFile"
 
-  cmp -s "$filename" "$targetFile" > /dev/null
-  comp_value=$?
-  #echo "comp_value is $comp_value"
-  if [ $comp_value -eq 1 ]; then
+  if cmp -s "$filename" "$targetFile"; then
+    echo ".... $filename is identical, skipping backup"
+  else
     backupFile "$filename"
+    putFile "$filename"
   fi
-
-  putFile "$filename"
 }
 
 createBackupDir
 
-installFile ".bashrc"
-installFile ".bash_profile"
-installFile ".bash_aliases"
-installFile ".bash_exports"
-installFile ".bash_functions"
-installFile ".bash_logout"
-installFile ".bash_options"
+for file in .bashrc .bash_profile .bash_aliases .bash_exports .bash_functions .bash_logout .bash_options; do
+  installFile "$file"
+done
 
+# Conditionally install .bash_private if it does not exist
+if [ ! -f "${HOME}/.bash_private" ]; then
+  echo '.... installing .bash_private'
+  printf "# .bash_private should NEVER be committed\n# Define private variables below.\n" > "${HOME}/.bash_private"
+else
+  echo '.... skipping .bash_private installation'
+fi
+
+# Conditionally install .bash_custom if it does not exist
 if [ ! -f "${HOME}/.bash_custom" ]; then
-  echo 'installing .bash_custom'
-  cat > ~/.bash_custom << EOF
-#
-# Place any client site custom profile settings here. The
-# install script will only copy this if it does not already exist.
-#
+  echo '.... installing .bash_custom'
+  printf "# CUSTOMER SPECIFIC BASH SETTINGS\n# Place any client site custom profile settings here. The\n# install script will only copy this if it does not already exist.\n" > "${HOME}/.bash_custom"
+else
+  echo '.... skipping .bash_custom installation'
+fi
 
+# Conditionally install .gitignore_global if it does not exist
+if [ ! -f "${HOME}/.gitignore_global" ]; then
+  echo '.... installing .gitignore_global'
+  cat > "${HOME}/.gitignore_global" << EOF
+  printf "# FILES THAT ARE GLOBALLY IGNORED BY GIT\nbash_private\n" > "${HOME}/.gitignore_global"
 EOF
+else
+  echo '.... skipping .gitignore_global installation'
 fi
 
 echo
